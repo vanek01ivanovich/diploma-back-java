9bcb961ae364ed170b42d5a0c6a3833beccec85b
Ivan <ivan.kuchkov17@gmail.com>
2021-02-02 20:41:59 +0200

    first commit

---
+++ src/main/java/ua/kpi/diploma/controltestinghub/ControlTestingHubApplication.java
@@ -0,0 +1,13 @@
+package ua.kpi.diploma.controltestinghub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlTestingHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlTestingHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/kpi/diploma/controltestinghub/ControlTestingHubApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kpi.diploma.controltestinghub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlTestingHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
