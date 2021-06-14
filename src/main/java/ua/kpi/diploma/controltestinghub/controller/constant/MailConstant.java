bddfadd44b15c5b6ca589ddec1d7a4a3bf7d5ebc
Vanek Kuchkov <55912342+vanek01ivanovich@users.noreply.github.com>
2021-06-14 23:40:47 +0300

    Update MailConstant.java

--- src/main/java/ua/kpi/diploma/controltestinghub/controller/constant/MailConstant.java
+++ src/main/java/ua/kpi/diploma/controltestinghub/controller/constant/MailConstant.java
@@ -2,6 +2,6 @@ package ua.kpi.diploma.controltestinghub.controller.constant;
 
 public interface MailConstant {
     String EMAIL = "io72.fiot.kpi@gmail.com";
-    String EMAIL_PASS = "kpi72-best001";
+    String EMAIL_PASS = "";
     String EMAIL_THEME = "Activate your account for testing app";
 }
